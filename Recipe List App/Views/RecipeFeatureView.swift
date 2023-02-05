cf4382f32efcc0370e12663af023639f84f10663
Tom Erik Heggedal <theggedal@gmail.com>
2023-02-05 23:05:57 +0100

    added the environment object

---
+++ Recipe List App/Views/RecipeFeatureView.swift
@@ -0,0 +1,23 @@
+//
+//  ReciepeFeatureView.swift
+//  Recipe List App
+//
+//  Created by Tom Erik Heggedal on 2/5/23.
+//
+
+import SwiftUI
+
+struct RecipeFeatureView: View {
+    
+    @EnvironmentObject var model:RecipeModel
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeFeatureView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeFeatureView()
+    }
+}
